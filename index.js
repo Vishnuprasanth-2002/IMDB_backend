5877d1c2fd97e55e7dcdec3c6ec559095273d310
Vishnuprasanth-2002 <s.vishnuprasanth14@gmail.com>
2023-12-08 00:31:48 +0530

    users completed

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const config = require("./config/config");
 const { models, Sequelize } = require("./config/sequelize-config");
+const userRouter = require("./routes/users.routes");
 
 // create application/json parser
 const jsonParser = bodyParser.json();
@@ -12,6 +13,7 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.use(jsonParser);
 app.use(urlencodedParser);
+app.use("/", userRouter);
 
 app.listen(config.port, config.host, () => {
   console.log(`Server running at http://${config.host}:${config.port}/`);
